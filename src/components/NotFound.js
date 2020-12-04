590a49bee1ea896bb38f9312cbd8dc7a63ae020b
shazy89 <erdo.shazy123@gmail.com>
2020-12-04 11:18:25 -0500

    deployed

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,13 +2,14 @@ import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import img from '../assets/images/404.png'
 
 const NotFound = () => {
     return (
         <div>
       <CssBaseline />
       <Container  maxWidth=" xs ,sm , md, lg, xs">
-        <Typography component="div" style={{ backgroundImage: 'url(https://res.cloudinary.com/dytheecsk/image/upload/v1607098287/portfolio/404_v2_yb62oq.png)', height: '100vh' }} />
+        <Typography component="div" style={{ backgroundImage: `url(${img})`, height: '100vh' }} />
       </Container>
         </div>
     )
