de5ecf4be438ab8cb913d3a9f7cb681a31ecc63b
shazy89 <erdo.shazy123@gmail.com>
2020-12-04 11:12:16 -0500

    added 404 page not found

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -6,7 +6,10 @@ import Container from '@material-ui/core/Container';
 const NotFound = () => {
     return (
         <div>
-
+      <CssBaseline />
+      <Container  maxWidth=" xs ,sm , md, lg, xs">
+        <Typography component="div" style={{ backgroundImage: 'url(https://res.cloudinary.com/dytheecsk/image/upload/v1607098287/portfolio/404_v2_yb62oq.png)', height: '100vh' }} />
+      </Container>
         </div>
     )
 }
