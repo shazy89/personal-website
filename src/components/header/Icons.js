610e40bffadd46c0d2ed49acf24c65f7c888dd04
shazy89 <erdo.shazy123@gmail.com>
2021-03-11 18:47:20 -0500

    added animation trigger code

--- src/components/header/Icons.js
+++ src/components/header/Icons.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import EmailIcon from "@material-ui/icons/Email";
@@ -6,11 +6,38 @@ import FacebookIcon from "@material-ui/icons/Facebook";
 import InstagramIcon from "@material-ui/icons/Instagram";
 
 const Icons = () => {
+  const [triger, setTriger] = useState(false);
+  const [anim, setAnim] = useState("");
+  const [animations, setAnimations] = useState({
+    animationFinished: true,
+    showAnimationStartLabel: false,
+  });
+  const triggerAnimation = () => (triger ? setTriger(false) : setTriger(true));
+
+  const onAnimationStart = () => {
+    setAnimations({
+      animationFinished: false,
+      showAnimationStartLabel: true,
+    });
+  };
+
+  const onAnimationEnd = () => {
+    setAnimations({
+      animationFinished: true,
+      showAnimationStartLabel: false,
+    });
+  };
+
   return (
     <div className="header__socialmedia-icons">
       <a
-        className="header__socialmedia-link"
+        onClick={triggerAnimation}
+        className={`header__socialmedia-link ${
+          triger ? "triger-animation" : ""
+        }`}
         href="mailto:erdo.shazy123@gmail.com"
+        onAnimationEnd={onAnimationEnd}
+        onAnimationStart={onAnimationStart}
       >
         <EmailIcon style={{ fontSize: "4.3rem" }} />
       </a>
