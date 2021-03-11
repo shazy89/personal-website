d8ac11e5c3aeec9e13454b98ae8caf866fc1b84b
shazy89 <erdo.shazy123@gmail.com>
2021-03-11 08:41:36 -0500

    React is redy to go

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import SN from "./components/sidenav/SN";
+import "./css/style.css";
+//import { BrowserRouter as Router } from "react-router-dom";
+//import SN from "./components/sidenav/SN";
 
-function App() {
-  return (
-    <div>
-      <Router>
-        <SN />
-      </Router>
-    </div>
-  );
-}
+const App = () => {
+  return <div className="library">HELLO WORLD</div>;
+};
 export default App;
