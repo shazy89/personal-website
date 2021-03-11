39f3776cddec1771d17c1c16bf1d7c5987c2cb36
shazy89 <erdo.shazy123@gmail.com>
2021-03-11 09:53:18 -0500

    added the header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./css/style.css";
+import Header from "./components/header/Header";
 //import { BrowserRouter as Router } from "react-router-dom";
 //import SN from "./components/sidenav/SN";
 
 const App = () => {
-  return <div className="library">HELLO WORLD</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 export default App;
