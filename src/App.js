152cf12f04b798930b9b7d72cf826581de96571b
shazy89 <erdo.shazy123@gmail.com>
2021-03-12 08:55:08 -0500

    display de video

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./css/style.css";
 import Header from "./components/header/Header";
 import Navigation from "./components/navbar/Navbar";
+import Portfolio from "./components/portfolio/Portfolio";
 //import { BrowserRouter as Router } from "react-router-dom";
 //import SN from "./components/sidenav/SN";
 
@@ -10,6 +11,7 @@ const App = () => {
     <>
       <Navigation />
       <Header />
+      <Portfolio />
     </>
   );
 };
--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import video from "../../assets/images/portfolio/coding.mp4";
+import video1 from "../../assets/images/portfolio/Coding_technology.mp4";
 const Portfolio = () => {
-  return <section className="section-portfolio"></section>;
+  return (
+    <section className="section-portfolio">
+      <div class="bg-video">
+        <video class="bg-video__content" autoPlay muted loop>
+          <source src={video1} type="video/mp4" />
+          Your browser is not supported!
+        </video>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
