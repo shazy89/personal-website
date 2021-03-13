233dd1d85ecba1dd71fd3ed14621a76ad0f225ef
shazy89 <erdo.shazy123@gmail.com>
2021-03-13 16:48:35 -0500

    card name header

--- src/components/card/CardHeading.js
+++ src/components/card/CardHeading.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const CardHeading = () => {
-  return <div className="card__heading"></div>;
+  return (
+    <div className="card__heading">
+      <h4 className="card__heading__text">
+        <span className="card__heading__text__span">HEY YOU</span>
+      </h4>
+      <div className="card__heading__img">
+        <img className="card__heading__img-1" src="" alt="" />
+        <img className="card__heading__img-2" src="" alt="" />
+        <img className="card__heading__img-3" src="" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default CardHeading;
