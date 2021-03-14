24b54903d2726a1e2a9599b65a229f06a11b11c2
shazy89 <erdo.shazy123@gmail.com>
2021-03-14 11:50:46 -0400

    hover effect to the link

--- src/components/card/CardFooter.js
+++ src/components/card/CardFooter.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const CardFooter = () => {
-  return <div className="card__footer"></div>;
+  return (
+    <div className="card__footer">
+      <a className="card__link" href="">
+        hey
+      </a>
+      <a className="card__link" href="">
+        you{" "}
+      </a>
+      <a className="card__link" href="">
+        ok
+      </a>
+    </div>
+  );
 };
 
 export default CardFooter;
