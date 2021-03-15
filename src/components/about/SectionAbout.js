40761de663d152a8a023f3d70b7884e289395498
shazy89 <erdo.shazy123@gmail.com>
2021-03-15 19:47:16 -0400

    about me section

--- src/components/about/SectionAbout.js
+++ src/components/about/SectionAbout.js
@@ -6,9 +6,9 @@ const SectionAbout = () => {
       <div className="about-me__container">
         <div className="about-me__img"></div>
 
-        <div className="about-me__content">
+        <div className="about-me__content u-margin-top-huge ">
           <h3 className="about-me__header">About Me</h3>
-          <p className="about-me__paragraph">
+          <p className="about-me__paragraph u-margin-top-small">
             {" "}
             Hi! I’m Erdoan but I go by Ed.  I’ve always been interested in
             software engineering and recently decided to change my career and
@@ -16,7 +16,7 @@ const SectionAbout = () => {
             in writing or debugging code. I can truly say that I am passionate
             about writing code and will not give up until something is perfect.
           </p>
-          <p className="about-me__paragraph">
+          <p className="about-me__paragraph u-margin-top">
             {" "}
             When I’m not coding, you can find me either playing video games or
             gone skiing if it’s wintertime. I love talking about motorcycles and
