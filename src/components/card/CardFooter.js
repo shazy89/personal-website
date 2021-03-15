d6f78e51ea92b5632034f247866c87560b455bd1
shazy89 <erdo.shazy123@gmail.com>
2021-03-15 16:52:18 -0400

    commit before switching to main branch

--- src/components/card/CardFooter.js
+++ src/components/card/CardFooter.js
@@ -1,24 +1,24 @@
 import React from "react";
 
-const CardFooter = ({ handleClick, github, mobGithub, demo }) => {
+const CardFooter = ({ github, mobGithub, demo }) => {
   return (
     <div className="card__footer">
-      <a
-        className="card__link"
-        href="https://www.youtube.com/watch?v=IEPtg8YQBbI&list=PL0zbnzgKNis1xNPhk3jmGx_KhQgK1SzaS&index=8&t=4s"
-        target="_blank"
-        rel="noreferrer"
-      >
+      <a className="card__link" href={demo} target="_blank" rel="noreferrer">
         Demo
       </a>
-      <a
-        className="card__link"
-        href={`https://github.com/WICG/EventListenerOptions/blob/gh-pages/explainer.md`}
-        target="_blank"
-        rel="noreferrer"
-      >
+      <a className="card__link" href={github} target="_blank" rel="noreferrer">
         GitHub{" "}
       </a>
+      {mobGithub && (
+        <a
+          className="card__link"
+          href={mobGithub}
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub MobileApp{" "}
+        </a>
+      )}
     </div>
   );
 };
