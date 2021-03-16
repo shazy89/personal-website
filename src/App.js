cb1f9d00e73d55d9bc25ab9030a02e4d82e27fdf
shazy89 <erdo.shazy123@gmail.com>
2021-03-16 08:39:10 -0400

    display the hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
+import Media from "react-media";
 import "./css/style.css";
 import Header from "./components/header/Header";
 import Navigation from "./components/navbar/Navbar";
+import Hamburger from "./components/navbar/HamburgerMenu";
 import Portfolio from "./components/portfolio/Portfolio";
 import SectionAbout from "./components/about/SectionAbout";
 
 const App = () => {
   return (
     <>
-      <Navigation />
+      <Media query="(max-width: 899px)">
+        <Hamburger />
+      </Media>
+      <Media query="(min-width: 900px)">
+        <Navigation />
+      </Media>
+
       <Header />
       <Portfolio />
       <SectionAbout />
