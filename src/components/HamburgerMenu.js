03f95dfd7a6ce6cdb7e0c432ecffa45451da5c09
shazy89 <erdo.shazy123@gmail.com>
2021-03-16 08:24:34 -0400

    navigation hamburger menu

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 const HamburgerMenu = () => {
-  return <div className="hamburger"></div>;
+  return (
+    <div className="hamburger">
+      <button className="hamburger__button"></button>
+      <div className="hamburger__background">
+        <nav className="hamburger__nav">
+          <ul className="hamburger__list">
+            <li className="hamburger__item">
+              <a href="#section_portfolio" className="navigation__link">
+                Portfolio
+              </a>
+            </li>
+            <li className="hamburger__item">
+              <a href="#section_portfolio" className="navigation__link">
+                Portfolio
+              </a>
+            </li>
+            <li className="hamburger__item">
+              <a href="#section_portfolio" className="navigation__link">
+                Portfolio
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 };
 
 export default HamburgerMenu;
