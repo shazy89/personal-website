36c016f1899ae26ec7a686f645bc4e225decc36e
shazy89 <erdo.shazy123@gmail.com>
2021-03-18 17:22:43 -0400

    responsive header section, reduced size of the images

--- src/components/header/Logo.js
+++ src/components/header/Logo.js
@@ -1,13 +1,23 @@
 import React from "react";
+import Media from "react-media";
 
 const Logo = () => {
   return (
     <div className="header__logo-box">
-      <img
-        src="https://res.cloudinary.com/dytheecsk/image/upload/v1615476201/ES-logos_black_opvfah.png"
-        alt="Logo"
-        className="header__logo"
-      />
+      <Media query="(min-width: 800px)">
+        <img
+          src="https://res.cloudinary.com/dytheecsk/image/upload/c_scale,w_350/v1615476201/ES-logos_black_opvfah.png"
+          alt="Logo"
+          className="header__logo"
+        />
+      </Media>
+      <Media query="(max-width: 800px)">
+        <img
+          src="https://res.cloudinary.com/dytheecsk/image/upload/c_scale,w_200/v1615476201/ES-logos_black_opvfah.png"
+          alt="Logo"
+          className="header__logo"
+        />
+      </Media>
     </div>
   );
 };
