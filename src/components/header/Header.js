9a1826fc69516cbf47fd9dfb374deb9dc16ec0df
Ed <erdo.shazy123@gmail.com>
2021-10-11 08:34:03 -0400

    fixed the text issue

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,17 @@
-import React from "react";
-import Logo from "./Logo";
-import Icons from "./Icons";
-import HeaderText from "./HeaderText";
+import React from 'react';
+import Logo from './Logo';
+import Icons from './Icons';
+import HeaderText from './HeaderText';
 
 const Header = () => {
   return (
-    <>
+    <div>
       <Logo />
       <header className="header">
         <HeaderText />
       </header>
       <Icons />
-    </>
+    </div>
   );
 };
 
