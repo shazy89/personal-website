1f01fb274bf1c0f685dc37a26073a7b729b1ef99
Ed <erdo.shazy123@gmail.com>
2021-11-18 11:23:42 -0500

    resume update

--- src/components/about/SectionAbout.js
+++ src/components/about/SectionAbout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const SectionAbout = () => {
   return (
@@ -15,7 +15,7 @@ const SectionAbout = () => {
         <div className="about-me__content u-margin-top-huge ">
           <h3 className="about-me__header">About Me</h3>
           <p className="about-me__paragraph u-margin-top-small">
-            {" "}
+            {' '}
             Hi! I’m Erdoan but I go by Ed.  I’ve always been interested in
             software engineering and recently decided to change my career and
             dive into coding! It’s an exciting journey and I enjoy getting lost
@@ -23,7 +23,7 @@ const SectionAbout = () => {
             about writing code and will not give up until something is perfect.
           </p>
           <p className="about-me__paragraph u-margin-top">
-            {" "}
+            {' '}
             When I’m not coding, you can find me either playing video games or
             gone skiing if it’s wintertime. I love talking about motorcycles and
             the adrenaline it gives me when riding them!
